4cf6bf270bd3c67195388bbe3ee2c4da77ec2d2b
Thilo Berger <thilob@gmx.net>
2025-01-11 00:51:49 +0100

    neues DB-Createskript erstellt.

--- db-createDatabase.sql
+++ db-createDatabase.sql
@@ -1,42 +1,76 @@
--- Schritt 1: Benutzer erstellen
--- Notwendig: Account mit Superuser-Eigenschaften, z.B. user: postgres
--- Role: cebiusdaten
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.3
+-- Dumped by pg_dump version 16.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 DROP DATABASE IF EXISTS cebiusdaten;
-DROP ROLE IF EXISTS cebiusdaten;
+--
+-- Name: cebiusdaten; Type: DATABASE; Schema: -; Owner: cebiusdaten
+--
 
-CREATE ROLE cebiusdaten WITH
-  LOGIN
-  NOSUPERUSER
-  INHERIT
-  NOCREATEDB
-  NOCREATEROLE
-  NOREPLICATION
-  ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:5Mq5l+WK6btSCYz5Y/oiAQ==$RAzWRLbYWb/LqRo03UN6uTbUbfbX4YBYVZIIkgPLc4w=:TqRN/9cLf9yhZVfTJkfhBtKczgsvLInVolXKEn8NoQ4=';
+CREATE DATABASE cebiusdaten WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'C.UTF-8';
 
-  -- Schritt 2: Datenbank erstellen
-  -- Database: cebiusdaten
 
-CREATE DATABASE cebiusdaten
-    WITH
-    OWNER = cebiusdaten
-    ENCODING = 'UTF8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+ALTER DATABASE cebiusdaten OWNER TO cebiusdaten;
 
-GRANT CREATE, CONNECT ON DATABASE cebiusdaten TO cebiusdaten;
-GRANT TEMPORARY ON DATABASE cebiusdaten TO cebiusdaten WITH GRANT OPTION;
 \connect cebiusdaten
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 --
 -- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
 --
+
 CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 
 
--- Schritt 3: Tabellen und andere Objekte anlegen
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: 
+--
 
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry and geography spatial types and functions';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
 --
 -- Name: gebref; Type: TABLE; Schema: public; Owner: cebiusdaten
 --
@@ -72,85 +106,6 @@ CREATE TABLE public.gebref (
 ALTER TABLE public.gebref OWNER TO cebiusdaten;
 
 --
--- Name: gebref.geom25832; Type: TABLE; Schema: public; Owner: cebiusdaten
---
-
-CREATE TABLE public."gebref.geom25832" (
-    id_0 integer NOT NULL,
-    geom25832 public.geometry(Point,25832)
-);
-
-
-ALTER TABLE public."gebref.geom25832" OWNER TO cebiusdaten;
-
---
--- Name: gebref.geom25832_id_0_seq; Type: SEQUENCE; Schema: public; Owner: cebiusdaten
---
-
-CREATE SEQUENCE public."gebref.geom25832_id_0_seq"
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public."gebref.geom25832_id_0_seq" OWNER TO cebiusdaten;
-
---
--- Name: gebref.geom25832_id_0_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: cebiusdaten
---
-
-ALTER SEQUENCE public."gebref.geom25832_id_0_seq" OWNED BY public."gebref.geom25832".id_0;
-
-
---
--- Name: gmadressen; Type: TABLE; Schema: public; Owner: cebiusdaten
---
-
-CREATE TABLE public.gmadressen (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    gemeinde_stadt character varying,
-    ortsteil character varying,
-    strassenschluessel character varying,
-    strassenname character varying,
-    hsnr character varying,
-    ostwert character varying,
-    nordwert character varying,
-    nummernteil character varying,
-    buchstaben character varying,
-    geom25832 public.geometry(Point,25832),
-    geom31466 public.geometry(Point,31466)
-);
-
-
-ALTER TABLE public.gmadressen OWNER TO cebiusdaten;
-
---
--- Name: gebref.geom25832 id_0; Type: DEFAULT; Schema: public; Owner: cebiusdaten
---
-
-ALTER TABLE ONLY public."gebref.geom25832" ALTER COLUMN id_0 SET DEFAULT nextval('public."gebref.geom25832_id_0_seq"'::regclass);
-
-
---
--- Name: gebref.geom25832 gebref.geom25832_pkey; Type: CONSTRAINT; Schema: public; Owner: cebiusdaten
---
-
-ALTER TABLE ONLY public."gebref.geom25832"
-    ADD CONSTRAINT "gebref.geom25832_pkey" PRIMARY KEY (id_0);
-
-
---
--- Name: gmadressen gmadressem_pkey; Type: CONSTRAINT; Schema: public; Owner: cebiusdaten
---
-
-ALTER TABLE ONLY public.gmadressen
-    ADD CONSTRAINT gmadressem_pkey PRIMARY KEY (id);
-
-
---
 -- Name: idx_gemeinde; Type: INDEX; Schema: public; Owner: cebiusdaten
 --
 
@@ -170,3 +125,4 @@ GRANT TEMPORARY ON DATABASE cebiusdaten TO cebiusdaten WITH GRANT OPTION;
 --
 -- PostgreSQL database dump complete
 --
+
